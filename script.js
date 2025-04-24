5ad5d9404e8c90cd962bfe86cf9449ffcbdc91d3
Ian Seki <ian.seki@ufu.br>
2025-04-24 10:07:10 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let position = 0;
+const carousel = document.getElementById("carousel");
+
+function moveCarousel(direction) {
+  const itemWidth = carousel.querySelector(".carousel-item").offsetWidth + 16; // item + space
+  const maxPosition = carousel.children.length - 1;
+  position = Math.min(Math.max(position + direction, 0), maxPosition);
+  carousel.scrollTo({ left: position * itemWidth, behavior: "smooth" });
+}
