560d5ac9c7a15241124632f23eadb5adb723d564
Ian Seki <ian.seki@ufu.br>
2025-04-24 12:53:46 -0300

    Alterações no Estilo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let position = 0;
 const carousel = document.getElementById("carousel");
 
 function moveCarousel(direction) {
-  const itemWidth = carousel.querySelector(".carousel-item").offsetWidth + 16; // item + space
+  const itemWidth = carousel.querySelector(".carousel-item").offsetWidth + 16;
   const maxPosition = carousel.children.length - 1;
   position = Math.min(Math.max(position + direction, 0), maxPosition);
   carousel.scrollTo({ left: position * itemWidth, behavior: "smooth" });
